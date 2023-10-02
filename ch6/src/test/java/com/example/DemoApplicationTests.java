fd88ba7e02fe6fa52220a04a3a0e0c60f7fc59e7
ggalloro <galloro.giovanni@gmail.com>
2023-10-02 11:05:31 +0200

    Chgd test code to simplify - roll bck from here

--- ch6/src/test/java/com/example/DemoApplicationTests.java
+++ ch6/src/test/java/com/example/DemoApplicationTests.java
@@ -1,28 +1,13 @@
-package com.example;
+import com.example.HelloController;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-@SpringBootTest
-@AutoConfigureMockMvc(printOnlyOnFailure = false)
-class DemoApplicationTests {
-
-	@Autowired
-	private MockMvc mvc;
+public class HelloControllerTest {
 
 	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-        .andExpect(content().string(equalTo("scd-on-gcp app running in target: local !!")));
+	public void testHello() {
+		HelloController controller = new HelloController();
+		String response = controller.hello();
+		assertEquals("scd-on-gcp app running in target: local !!", response);
 	}
 }
