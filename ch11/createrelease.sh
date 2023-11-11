3c35b9e53752b234816cca6e54c9449e6880c499
ggalloro <galloro.giovanni@gmail.com>
2023-11-11 18:43:10 +0100

    Fixed values for region, cluster names, repo

--- ch11/createrelease.sh
+++ ch11/createrelease.sh
@@ -2,10 +2,10 @@
 echo "Building image for 1st release"
 echo
 ###
-skaffold build --default-repo=yourrepo --file-output=artifacts.json
+skaffold build --default-repo=us-central1-docker.pkg.dev/$PROJECT_ID/scdbook-repo --file-output=artifacts.json
 ###
 echo "Creating 1st release"
 echo
-gcloud deploy releases create first-release --delivery-pipeline scd-on-gcp-pipeline --region yourregion --build-artifacts artifacts.json
+gcloud deploy releases create first-release --delivery-pipeline scd-on-gcp-pipeline --region us-central1 --build-artifacts artifacts.json
 ###
 echo "Release creation completed, please promote and approve your release manually"
--- ch11/setup.sh
+++ ch11/setup.sh
@@ -6,29 +6,6 @@ echo "Enter your GCP project ID:"
 read yourproject
 sed -i.bak "s/yourproject/$yourproject/g" delivery-pipeline.yaml
 ###
-echo "Enter the location (zone or region) of your GKE clusters:"
-read yourlocation
-sed -i.bak "s/yourlocation/$yourlocation/g" delivery-pipeline.yaml
-###
-echo "Enter the name choosen for your GKE qa cluster (for example qa-cluster):"
-read qacluster
-sed -i.bak "s/qacluster/$qacluster/g" delivery-pipeline.yaml
-###
-echo "Enter the name choosen for your GKE prod cluster (for example prod-cluster):"
-read prodcluster
-sed -i.bak "s/prodcluster/$prodcluster/g" delivery-pipeline.yaml
-###
-echo "Enter the name of your GCP region (the one you will create the delivery pipeline in):"
-read yourregion
-sed -i.bak "s/yourregion/$yourregion/g" build-qa.yaml
-sed -i.bak "s/yourregion/$yourregion/g" release-prod.yaml
-sed -i.bak "s/yourregion/$yourregion/g" createrelease.sh
-###
-echo "Enter the full path of your Artifact Registry repository:"
-read yourrepo
-sed -i.bak "s,yourrepo,$yourrepo,g" build-qa.yaml
-sed -i.bak "s,yourrepo,$yourrepo,g" createrelease.sh
-###
 rm -rf delivery-pipeline.yaml.bak
 rm -rf build-qa.yaml.bak
 rm -rf release-prod.yaml.bak
