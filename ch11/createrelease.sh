056816eed7999ab83dad7dec8f001c58cf6ab406
ggalloro <galloro.giovanni@gmail.com>
2023-11-12 21:39:20 +0100

    Attempt to set up Kritis attestation

--- ch11/createrelease.sh
+++ ch11/createrelease.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-echo "Building image for 1st release"
-echo
-###
-skaffold build --push=true --default-repo=us-central1-docker.pkg.dev/$PROJECT_ID/scdbook-repo --file-output=artifacts.json
-###
 echo "Creating 1st release"
 echo
-gcloud deploy releases create first-release --delivery-pipeline scd-on-gcp-pipeline --region us-central1 --build-artifacts artifacts.json
+###
+gcloud builds submit --substitutions=_KMS_KEY_NAME=projects/$PROJECT_ID/locations/global/keyRings/binauthz-ring/cryptoKeys/binauthz-key/cryptoKeyVersions/1,_KMS_DIGEST_ALG=SHA256,_NOTE_NAME=projects/$PROJECT_ID/notes/vuln-scanner-note --region=us-central1 --project=$PROJECT_ID --config first-release.yaml
 ###
 echo "Release creation completed, please promote and approve your release manually"
