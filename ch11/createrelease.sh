711b758cd25be07be929739db1a1a913a125512a
ggalloro <galloro.giovanni@gmail.com>
2023-11-13 19:02:16 +0100

    Fixes 4 img scan (+kms keys to be rolled back)

--- ch11/createrelease.sh
+++ ch11/createrelease.sh
@@ -2,6 +2,6 @@
 echo "Creating 1st release"
 echo
 ###
-gcloud builds submit --substitutions=_KMS_KEY_NAME=projects/$PROJECT_ID/locations/global/keyRings/binauthz-ring/cryptoKeys/binauthz-key/cryptoKeyVersions/1,_KMS_DIGEST_ALG=SHA256,_NOTE_NAME=projects/$PROJECT_ID/notes/vuln-scanner-note --region=us-central1 --project=$PROJECT_ID --config first-release.yaml
+gcloud builds submit --substitutions=_KMS_KEY_NAME=projects/$PROJECT_ID/locations/global/keyRings/binauthz/cryptoKeys/vulnz-signer/cryptoKeyVersions/1,_KMS_DIGEST_ALG=SHA512,_NOTE_NAME=projects/$PROJECT_ID/notes/vuln-scanner-note --region=us-central1 --project=$PROJECT_ID --config first-release.yaml
 ###
 echo "Release creation completed, please promote and approve your release manually"
